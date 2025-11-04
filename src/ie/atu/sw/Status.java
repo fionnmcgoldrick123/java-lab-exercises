152b8f6543f720a178dad7de81f4e0071109ea0a
fionn mcgoldrick <fionnmc123@gmail.com>
2025-11-04 00:35:58 +0000

    add java code stubs to the project to begin

---
+++ src/ie/atu/sw/Status.java
@@ -0,0 +1,5 @@
+package ie.atu.sw;
+
+public enum Status {
+	Luxury, Premium, Economy, Basic, Steerage;
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module atu.software {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires transitive javafx.graphics;
+	
+	
+	exports ie.atu.sw;
+}
