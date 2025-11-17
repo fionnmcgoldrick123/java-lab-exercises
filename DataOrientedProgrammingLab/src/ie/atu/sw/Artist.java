3f13bf363bcc71cf28340680b17d03ca21ccb2ef
fionn mcgoldrick <fionnmc123@gmail.com>
2025-11-17 10:45:12 +0000

    add canonical constructors for Artist and Attendee classes

--- DataOrientedProgrammingLab/src/ie/atu/sw/Artist.java
+++ DataOrientedProgrammingLab/src/ie/atu/sw/Artist.java
@@ -1,11 +1,20 @@
 package ie.atu.sw;
 
+import static java.util.Objects.*;
+
+import java.util.UUID;
 public record Artist(String id, String name, Genre genre) {
 
 	enum Genre {BLUES, COUNTRY, FOLK, HIP_HOP,
 		JAZZ, METAL, POP, PUNK, ROCK, SOUL}
 	
+	public Artist(String name, Genre genre) {
+		this(UUID.randomUUID().toString(), name, genre);
+	}
+	
 	public Artist {
-		
+		requireNonNull(id);
+		requireNonNull(name);
+		requireNonNull(genre);
 	}
 }
--- DataOrientedProgrammingLab/src/ie/atu/sw/Attendee.java
+++ DataOrientedProgrammingLab/src/ie/atu/sw/Attendee.java
@@ -1,9 +1,19 @@
 package ie.atu.sw;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.List;
+import java.util.UUID;
 
 public record Attendee(String id, String name, String email, List<Ticket> tickets) {
+	
+	public Attendee(String name, String email, List<Ticket> tickets) {
+		this(UUID.randomUUID().toString(), name, email, tickets);
+	}
+	
 	public Attendee {
-		
+		requireNonNull(id);
+		requireNonNull(name);
+		requireNonNull(email);
 	}
 }
